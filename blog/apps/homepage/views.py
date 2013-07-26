e86a09923e9ceeb5663fe54a3d0830717e1b27eb
cecep17 <wandy@localhost.localdomain>
2013-07-26 18:02:38 +0700

    i has change a lot this blog

--- blog/apps/homepage/views.py
+++ blog/apps/homepage/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render_to_response
+from apps.data.models import Entry
 
 def index(request):
-	return render_to_response('homepage/index.html')
+	entries = Entry.objects.filter(published=True).order_by('-id')
+	ctx = {'entries':entries}
+	return render_to_response('homepage/index.html',ctx)
